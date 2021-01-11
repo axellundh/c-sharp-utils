ffbfca5ce3fb3491a4d3d25085702fd13f559b62
Axel Lundh <axel@axellundh.se>
2021-01-11 11:04:22 +0100

    add double parser

---
+++ axel-utils/axelutilsTests/Parsing/ParserTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using axelutils.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace axelutils.Parsing.Tests
+{
+    public class ParserTests
+    {
+        [Theory]
+        [InlineData("1,8", 1.8)]
+        [InlineData("186", 186.0)]
+        [InlineData("0,4", 0.4)]
+        [InlineData("0.4", 0.4)]
+        [InlineData("168541.41681", 168541.41681)]
+        public void StringToDoubleTest(string input, double expected)
+        {
+            double output = Parser.StringToDouble(input);
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [InlineData("1,8", 1.8)]
+        [InlineData("186", 186.0)]
+        [InlineData("0,4", 0.4)]
+        [InlineData("0.4", 0.4)]
+        [InlineData("168541.41681", 168541.41681)]
+        public void StringToDecimalTest(string input, decimal expected)
+        {
+            decimal output = Parser.StringToDecimal(input);
+            Assert.Equal(expected, output);
+        }
+    }
+}
---
+++ axel-utils/utils/Parsing/Parser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace axelutils.Parsing
+{
+    public class Parser
+    {
+        /// <summary>
+        /// Converts a string to double, respects diffrent culutre decimal deliminators
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static double StringToDouble(in string input)
+        {
+            double number;
+            CultureInfo culture;
+            try
+            {
+                culture = CultureInfo.CreateSpecificCulture("sv-SE");
+                number = double.Parse(input, culture);
+                return number;
+            }
+            catch (FormatException)
+            {
+                culture = CultureInfo.CreateSpecificCulture("en-US");
+                try
+                {
+                    number = double.Parse(input, culture);
+                    return number;
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"{culture.Name}: Unable to parse {input}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts a string to decimal, respects diffrent culutre decimal deliminators
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static decimal StringToDecimal(in string input)
+        {
+            decimal number;
+            CultureInfo culture;
+            try
+            {
+                culture = CultureInfo.CreateSpecificCulture("sv-SE");
+                number = decimal.Parse(input, culture);
+                return number;
+            }
+            catch (FormatException)
+            {
+                culture = CultureInfo.CreateSpecificCulture("en-US");
+                try
+                {
+                    number = decimal.Parse(input, culture);
+                    return number;
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"{culture.Name}: Unable to parse {input}");
+                }
+            }
+        }
+    }
+}
